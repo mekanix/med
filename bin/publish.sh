a8d34d82bd36cfabe844977a54de4120daa0cc92
Goran Mekić <meka@tilda.center>
2020-06-07 01:27:35 +0200

    Rename service

--- bin/publish.sh
+++ bin/publish.sh
@@ -7,5 +7,5 @@ PASSWORD=`cat ${PROJECT_ROOT}/publish_password.txt`
 
 cd "${PROJECT_ROOT}"
 rsync -P -avcl --delete-after build/ med.meka.rs:/usr/cbsd/jails-data/nginx-data/usr/local/www/med.meka.rs/
-rsync -P -avcl --delete-after --exclude=.git* --exclude=__pycache__ --exclude=Makefile --exclude=ansible --exclude=build --exclude=cbsd.conf --exclude=local_config.py --exclude=*.db --exclude=*.mk --exclude=requirements.yml --exclude=static --exclude=templates --exclude=tests services/backend/ med.meka.rs:/usr/cbsd/jails-data/medback-data/usr/home/deploy/repos/med/
+rsync -P -avcl --delete-after --exclude=.git* --exclude=__pycache__ --exclude=Makefile --exclude=ansible --exclude=build --exclude=cbsd.conf --exclude=local_config.py --exclude=*.db --exclude=*.mk --exclude=requirements.yml --exclude=static --exclude=templates --exclude=tests services/backend/ med.meka.rs:/usr/cbsd/jails-data/med-data/usr/home/deploy/repos/med/
 "${BIN_DIR}/publish" "${PASSWORD}"
