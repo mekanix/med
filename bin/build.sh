8cd954a5f2ce71e8dd76eeaf1ea85f55453f60ed
Goran Mekić <meka@tilda.center>
2020-06-13 15:40:28 +0200

    Cleanup

--- bin/build.sh
+++ bin/build.sh
@@ -9,15 +9,9 @@ export backend_app_name=${app_name}
 export frontend_app_name=${app_name}
 
 
-if [ -f /usr/local/bin/cbsd ]; then
-  sudo cbsd jexec user=devel "jname=${backend_app_name}back" /usr/src/bin/build.sh
-  sudo cbsd jexec user=devel "jname=${frontend_app_name}front" /usr/src/bin/build.sh
-else
-  ${PROJECT_ROOT}/services/backend/bin/build.sh
-  ${PROJECT_ROOT}/services/frontend/bin/build.sh
-fi
-
 rm -rf ${PROJECT_ROOT}/build
 mkdir ${PROJECT_ROOT}/build
+${PROJECT_ROOT}/services/backend/bin/collect.sh
+${PROJECT_ROOT}/services/frontend/bin/collect.sh
 cp -r "${PROJECT_ROOT}/services/backend/static" "${PROJECT_ROOT}/build/"
 cp -r ${PROJECT_ROOT}/services/frontend/build/* "${PROJECT_ROOT}/build/"
