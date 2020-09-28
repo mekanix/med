b8868461efb57922b1266d209130f5d8c017a9b6
Goran Mekić <meka@tilda.center>
2020-09-28 09:50:17 +0200

    Update freenit scripts

--- bin/download_repos.sh
+++ bin/download_repos.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 export BIN_DIR=`dirname $0`
-export PROJECT_ROOT=`readlink -f "${BIN_DIR}/.."`
+export PROJECT_ROOT="${BIN_DIR}/.."
 
 if [ ! -d "${PROJECT_ROOT}/services" ]; then
   mkdir "${PROJECT_ROOT}/services"
 fi
 
 if [ ! -d "${PROJECT_ROOT}/services/backend" ]; then
-  git clone https://github.com/bsidesns/backend "${PROJECT_ROOT}/services/backend"
+  git clone https://github.com/webbell/backend "${PROJECT_ROOT}/services/backend"
 fi
 if [ ! -d "${PROJECT_ROOT}/services/frontend" ]; then
-  git clone https://github.com/bsidesns/frontend "${PROJECT_ROOT}/services/frontend"
+  git clone https://github.com/webbell/frontend "${PROJECT_ROOT}/services/frontend"
 fi
